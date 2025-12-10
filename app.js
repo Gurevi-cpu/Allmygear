cbc9e07d6d4735525a5fc1b69f7efe0a9d9307ca
Ilya Gurevich <ilyagurevich@Ilyas-MacBook-Pro.local>
2025-12-10 17:09:31 +0300

    Add share button next to edit button in checklist toolbar

--- app.js
+++ app.js
@@ -4015,6 +4015,11 @@
                 <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a1 1 0 0 0 0-1.41l-2.34-2.34a1 1 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
               </svg>
             </button>
+            <button class="btn icon" data-action="share-checklist" data-id="${checklist.id}" title="Share checklist">
+              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/>
+              </svg>
+            </button>
             <button class="btn icon" data-action="copy-checklist" data-id="${checklist.id}" title="Copy checklist">
               <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/>
@@ -4863,6 +4868,21 @@
         renderGearItems(selectedCategories, itemIds)
         
         if(checklistModal) checklistModal.classList.remove('hidden')
+      }
+      return
+    }
+    
+    if(action === 'share-checklist'){
+      if (!SupabaseService.getCurrentUser()) {
+        alert('Please sign in to share checklists')
+        return
+      }
+      
+      const id = btn.dataset.id
+      const checklist = checklists.find(c => c.id === id)
+      
+      if(checklist){
+        openShareChecklistModal(checklist)
       }
       return
     }
